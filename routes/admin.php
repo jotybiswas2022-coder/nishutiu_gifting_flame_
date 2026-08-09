6bfe2315cef3160220fa7167adafa54a5b54b0b2
jotybiswas2022-coder <jotybiswas2022@gmail.com>
2026-08-09 20:42:46 +0600

    Add admin dashboard routes and AdminController

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 
 Route::prefix('admin')->middleware('admin')->group(function () {
-
+    Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::get('/contact', [AdminController::class, 'contacts'])->name('contact.index');
 });
